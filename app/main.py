a5780c38337abf44c74957badfc5c655edb0cf71
nvarabioff <nvarabioff@users.noreply.github.com>
2021-11-16 09:07:58 -0500

    Address PR comments, add TODOs.

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
 )
 
 # Just going to create a dictionary for now. Not sure how it would fit into current database.
-# Probably should add this to the database, but that might need further discussion.
+# TODO: Probably should add this to the database, but that might need further discussion.
 # key is first letter of postal code, value is province_code in db.
 postal_to_province = {
     'A': 23,    # Newfoundland and Labrador
@@ -70,37 +70,38 @@ def show_records(db: Session = Depends(get_db)):
     records = db.query(models.EnergyType).all()
     return records
 
+
 @app.get("/energy-profile/")
 def get_profile_from_postal(postal_code: Optional[str] = None, province: Optional[str] = None, db: Session = Depends(get_db)):
     records = {}
     # get postal code and province from energy_profiles depending on input parameters
     try:
-        if province != None:
+        if postal_code != None:
+            records['energy_profile_code'] = db.query(models.Province).\
+                filter(models.Province.province_code == postal_to_province[postal_code[0].upper()]).\
+                    first().energy_profile_code
+            records['province_name'] = db.query(models.Province).\
+                filter(models.Province.province_code == postal_to_province[postal_code[0].upper()]).\
+                    first().name
+        elif province != None and postal_code == None:
             records['province_name'] = db.query(models.Province).\
                 filter(models.Province.abbreviation == province.upper()).first().name
             records['energy_profile_code'] = db.query(models.Province).\
                 filter(models.Province.abbreviation == province.upper()).first().energy_profile_code
-        elif postal_code != None and province == None:
-            records['energy_profile_code'] = db.query(models.Province).\
-                filter(models.Province.province_id == postal_to_province[postal_code[0].upper()]).\
-                    first().energy_profile_code
-            records['province_name'] = db.query(models.Province).\
-                filter(models.Province.province_id == postal_to_province[postal_code[0].upper()]).\
-                    first().name
-        else:
-            return {"ERROR":"no data"}
+        else:   # TODO: Check if there is mismatch between province and postal code parameter
+            return {"ERROR":"no data"}  # TODO: Throw real error message
     except AttributeError:
-        return {"ERROR":"invalid parameter"}
+        return {"ERROR":"invalid parameter"}    # TODO: Throw real error message
     # get energy producers from that province
     records['sources'] = db.query(models.EnergySource, models.EnergyType).\
         filter(models.EnergySource.energy_type == models.EnergyType.energy_type).\
             filter(models.EnergySource.energy_profile_code == records['energy_profile_code']).all()
     # calculate percent renewable and percent non emitting
     # calculate emissions
-    mw_total = 1    # This data is in MW
+    mw_total = 0    # This data is in MW
     mw_renewable = 0
     mw_non_emitting = 0
-    records['emissions'] = 0    # This data will be kg CO2 per hour
+    records['emissions'] = 0    # This data will be kg CO2 emitted per hour
     for element in records['sources']:
         mw_total += element.EnergySource.quantity
         if element.EnergyType.is_renewable == True:
@@ -108,6 +109,6 @@ def get_profile_from_postal(postal_code: Optional[str] = None, province: Optiona
         if element.EnergyType.is_non_emitting == True:
             mw_non_emitting += element.EnergySource.quantity
         records['emissions'] += element.EnergySource.quantity * element.EnergyType.co2_per_kwh
-    records['percent_renewable'] = mw_renewable / mw_total
-    records['percent_non_emitting'] = mw_non_emitting / mw_total
+    records['percent_renewable'] = mw_renewable / mw_total if mw_total > 0 else 'no data'
+    records['percent_non_emitting'] = mw_non_emitting / mw_total if mw_total > 0 else 'no data'
     return records
